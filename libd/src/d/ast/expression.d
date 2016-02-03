UplinkCoder/sdc32-experimental

Implement more isExpressions

libd/src/d/ast/expression.d
@@ -493,15 +493,88 @@ class DollarExpression : AstExpression {
 }
 
 /**
+ * flavour of isExpression.
+ */
+enum IsKind {
+	Basic,
+	ConvertCompare,
+	ExactCompare,
+	Kind,
+	Qualifier,
+}
+/**
  * is expression.
  */
 class IsExpression : AstExpression {
 	AstType tested;
-	
+	IsKind isKind;
+
+	import d.ir.type:TypeKind;
+	union {
+		AstType against;
+		TypeKind typeKind;
+		TypeQualifier qualifier;
+	}
+
 	this(Location location, AstType tested) {
 		super(location);
 		
 		this.tested = tested;
+		this.isKind = IsKind.Basic;
+	}
+
+
+	this(Location location, AstType tested, bool isExact, AstType against) {
+		super(location);
+
+		this.tested = tested;
+		if (isExact) {
+			this.isKind = isKind.ExactCompare;
+		} else {
+			this.isKind = IsKind.ConvertCompare;
+		}
+		this.against = against;
+	}
+
+	this(Location location, AstType tested, TypeKind typeKind) {
+		super(location);
+		
+		this.tested = tested;
+		this.isKind = isKind.Kind;
+		this.typeKind = typeKind;
+	}
+
+	this(Location location, AstType tested, TypeQualifier qualifier) {
+		super(location);
+		
+		this.tested = tested;
+		this.isKind = IsKind.Qualifier;
+		this.qualifier = qualifier;
+	}
+
+	override string toString(const Context c) const {
+		import std.conv:to;
+		import std.string:toLower;
+		string result = "is(" ~ tested.toString(c);
+
+		final switch (isKind) with (IsKind) {
+		case ExactCompare :
+			result ~= " == " ~ against.toString(c);
+			goto case Basic;
+		case ConvertCompare :
+			result ~= " : " ~ against.toString(c);
+			goto case Basic;
+		case Kind :
+			result ~= " == " ~ to!string(typeKind).toLower;
+			goto case Basic;
+		case Qualifier :
+			result ~= " == " ~ to!string(qualifier).toLower;
+			goto case Basic;
+		case Basic :
+			result ~= ")";
+		}
+
+		return result;
 	}
 }
 

libd/src/d/parser/expression.d
@@ -860,28 +860,77 @@ private auto parseIsExpression(ref TokenRange trange) {
 	trange.match(TokenType.OpenParen);
 	
 	auto type = trange.parseType();
-	
+
+	IsExpression result = new IsExpression(location, type);
+
 	// Handle alias throw is expression.
-	if (trange.front.type == TokenType.Identifier) {
-		trange.popFront();
-	}
+//	if (trange.front.type == TokenType.Identifier) {
+//		trange.popFront();
+//	}
 	
 	switch(trange.front.type) with(TokenType) {
 		case Colon :
 			trange.popFront();
-			trange.parseType();
+			result.against = trange.parseType();
+			result.isKind = IsKind.ConvertCompare;
 			break;
-		
+
 		case EqualEqual :
 			trange.popFront();
-			
+	
 			switch(trange.front.type) {
-				case Struct, Union, Class, Interface, Enum, Function, Delegate :
-				case Super, Const, Immutable, Inout, Shared, Return :
+				import d.ir.type:TypeKind;
+				import d.ast.type:TypeQualifier;
+
+				case Struct :
+					result.typeKind = TypeKind.Struct;
+					goto setKindTypeKind;
+				case Union :
+					result.typeKind = TypeKind.Union;
+					goto setKindTypeKind;
+				case Class :
+					result.typeKind = TypeKind.Class;
+					goto setKindTypeKind;
+				case Interface :
+					result.typeKind = TypeKind.Interface;
+					goto setKindTypeKind;
+				case Enum :
+					result.typeKind = TypeKind.Enum;
+					goto setKindTypeKind;
+				case Function :
+					result.typeKind = TypeKind.Function;
+					goto setKindTypeKind;
+				case Delegate :
 					assert(0, "Not implemented.");
-				
+
+				setKindTypeKind :
+					trange.popFront();
+					result.isKind = IsKind.Kind;
+					break;
+
+				case Const :
+					result.qualifier = TypeQualifier.Const;
+					goto setKindQualifier;
+				case Immutable :
+					result.qualifier = TypeQualifier.Immutable;
+					goto setKindQualifier;
+				case Inout :
+					result.qualifier = TypeQualifier.Inout;
+					goto setKindQualifier;
+				case Shared :
+					result.qualifier = TypeQualifier.Shared;
+					goto setKindQualifier;
+				case Super, Return : 
+					assert(0, "Not implemented.");
+					
+				setKindQualifier :
+					trange.popFront();
+					result.isKind = IsKind.Qualifier;
+					break;
+
 				default :
-					trange.parseType();
+					result.against = trange.parseType();
+					result.isKind = IsKind.ExactCompare;
 			}
 			
 			break;
@@ -891,9 +940,10 @@ private auto parseIsExpression(ref TokenRange trange) {
 	}
 	
 	location.spanTo(trange.front.location);
+	result.location = location;
 	trange.match(TokenType.CloseParen);
-	
-	return new IsExpression(location, type);
+
+	return result;
 }
 
 /**

UplinkCoder/SDC

libd/src/d/ast/expression.d
@@ -493,15 +493,90 @@ class DollarExpression : AstExpression {
 }
 
 /**
+ * flavour of isExpression.
+ */
+enum IsKind {
+	Basic,
+	ConvertCompare,
+	ExactCompare,
+	Kind,
+	Qualifier,
+}
+/**
  * is expression.
  */
 class IsExpression : AstExpression {
 	AstType tested;
-	
+	IsKind isKind;
+
+	import d.ir.type:TypeKind;
+	union {
+		AstType against;
+		TypeKind typeKind;
+		TypeQualifier qualifier;
+	}
+
 	this(Location location, AstType tested) {
 		super(location);
 		
 		this.tested = tested;
+		this.isKind = IsKind.Basic;
+	}
+
+
+	this(Location location, AstType tested, bool isExact, AstType against) {
+		super(location);
+
+		this.tested = tested;
+		if (isExact) {
+			this.isKind = isKind.ExactCompare;
+		} else {
+			this.isKind = IsKind.ConvertCompare;
+		}
+		this.against = against;
+	}
+
+	this(Location location, AstType tested, TypeKind typeKind) {
+		super(location);
+		
+		this.tested = tested;
+		this.isKind = isKind.Kind;
+		this.typeKind = typeKind;
+	}
+
+	this(Location location, AstType tested, TypeQualifier qualifier) {
+		super(location);
+		
+		this.tested = tested;
+		this.isKind = IsKind.Qualifier;
+		this.qualifier = qualifier;
+	}
+
+	override string toString(const Context c) const {
+		import std.conv : to;
+		import std.string : toLower;
+		string result = "is(" ~ tested.toString(c);
+
+		final switch (isKind) with (IsKind) {
+			case ExactCompare :
+				result ~= " == " ~ against.toString(c);
+				break;
+			case ConvertCompare :
+				result ~= " : " ~ against.toString(c);
+				break;
+			case Kind :
+				result ~= " == " ~ to!string(typeKind).toLower;
+				break;
+			case Qualifier :
+				result ~= " == " ~ to!string(qualifier).toLower;
+				break;
+				
+			case Basic :
+				break;
+		}
+
+		result ~= ")";
+		return result ;
 	}
 }
 

libd/src/d/parser/expression.d
@@ -865,40 +865,97 @@ private auto parseIsExpression(ref TokenRange trange) {
 	trange.match(TokenType.OpenParen);
 	
 	auto type = trange.parseType();
-	
+
+	IsExpression result = new IsExpression(location, type);
+
 	// Handle alias throw is expression.
-	if (trange.front.type == TokenType.Identifier) {
-		trange.popFront();
-	}
+//	if (trange.front.type == TokenType.Identifier) {
+//		trange.popFront();
+//	}
 	
 	switch(trange.front.type) with(TokenType) {
 		case Colon :
 			trange.popFront();
-			trange.parseType();
+			result.against = trange.parseType();
+			result.isKind = IsKind.ConvertCompare;
 			break;
-		
+
 		case EqualEqual :
 			trange.popFront();
-			
+	
 			switch(trange.front.type) {
-				case Struct, Union, Class, Interface, Enum, Function, Delegate :
-				case Super, Const, Immutable, Inout, Shared, Return :
+				import d.ir.type : TypeKind;
+				import d.ast.type : TypeQualifier;
+
+				case Struct :
+					result.typeKind = TypeKind.Struct;
+					goto setKindTypeKind;
+				case Union :
+					result.typeKind = TypeKind.Union;
+					goto setKindTypeKind;
+				case Class :
+					result.typeKind = TypeKind.Class;
+					goto setKindTypeKind;
+				case Interface :
+					result.typeKind = TypeKind.Interface;
+					goto setKindTypeKind;
+				case Enum :
+					result.typeKind = TypeKind.Enum;
+					goto setKindTypeKind;
+				case Function :
+					result.typeKind = TypeKind.Function;
+					goto setKindTypeKind;
+				case Delegate :
 					assert(0, "Not implemented.");
-				
+
+				setKindTypeKind :
+					trange.popFront();
+					result.isKind = IsKind.Kind;
+					break;
+
+				case Const :
+					result.qualifier = TypeQualifier.Const;
+					goto setKindQualifier;
+				case Immutable :
+					result.qualifier = TypeQualifier.Immutable;
+					goto setKindQualifier;
+				case Inout :
+					result.qualifier = TypeQualifier.Inout;
+					goto setKindQualifier;
+				case Shared :
+					result.qualifier = TypeQualifier.Shared;
+					goto setKindQualifier;
+
+				case Super, Return : 
+					assert(0, "Not implemented.");
+					
+				setKindQualifier :
+					trange.popFront();
+
+					if (trange.front.type == TokenType.CloseParen) {
+						result.isKind = IsKind.Qualifier;
+					} else {
+						result.against = trange.parseType().qualify(result.qualifier);
+						result.isKind = IsKind.ExactCompare;
+					}
+					break;
+
 				default :
-					trange.parseType();
+					result.against = trange.parseType();
+					result.isKind = IsKind.ExactCompare;
 			}
 			
 			break;
-		
+			
 		default :
 			break;
 	}
 	
 	location.spanTo(trange.front.location);
+	result.location = location;
 	trange.match(TokenType.CloseParen);
-	
-	return new IsExpression(location, type);
+
+	return result;
 }
 
 /**